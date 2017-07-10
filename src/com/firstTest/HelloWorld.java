4ea99bea331c6630faffa94794accf5ff554f5ae
ajboard13 <aaronjboard@live.com>
2017-07-10 10:36:48 -0400

    My very first program using the spring framework

---
+++ src/com/firstTest/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.firstTest;
+
+/*
+Aaron Board
+7/10/2017
+ */
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message){
+        this.message  = message;
+    }
+    public void getMessage(){
+        System.out.println("Your Message : " + message);
+    }
+}
